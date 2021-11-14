8dedbe65c58065896f53a4fb710912016d3e4334
femonds <f.emonds@reply.de>
2021-11-14 02:24:24 +0100

    prod deployment

--- split-string.sh
+++ split-string.sh
@@ -1,18 +1,15 @@
 #!/bin/sh
+
+## option for sh / shell
+foo="udapeople-c7f7615"
+echo ${foo##*-}
+OldWorkflowID=${foo##*-}
+echo OldWorkflowID: "${OldWorkflowID}"
+
+### Option for bash
 #IN="udapeople-c7f7615"
 #IFS='-' read -ra ADDR <<< "$IN"
 #for i in "${ADDR[@]}"; do
 #  OldWorkflowID=$i
 #done
 #echo $OldWorkflowID
-
-foo="udapeople-c7f7615"
-echo ${foo##*-}
-OldWorkflowID=${foo##*-}
-echo OldWorkflowID: "${OldWorkflowID}"
-
-
- #             IFS='-' read -ra ADDR <<< "$IN"
- #             for i in "${ADDR[@]}"; do
- #               OldWorkflowID=$i
- #             done
